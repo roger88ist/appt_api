76ed84e2f8349365774ec0b6f321a720295d69a4
Rogerio Istiraneopulos <roger88ist@gmail.com>
2016-06-02 19:35:19 -0400

    dried up some of the repetitive code by creating methods in the private section of the controller.

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -3,34 +3,26 @@ class Api::AppointmentsController < ApplicationController
 
 	def index
 		appointments = Appointment.all
-		render status: 200, json: { 
-			status: 200,
-			appointments: appointments
-		}.to_json
+		render_json_success_with_appointment(appointments)
 	end
 
 	def show
 		appointment = Appointment.find(params[:id])
-		render status: 200, json: {
-			status: 200,
-			appointment: appointment
-		}.to_json
+		render_json_success_with_appointment(appointment)
 	end
 
 	def update
 		appointment = Appointment.find(params[:id])
-		if appointment.update(appointment_params)
-			appointment.end_time = appointment.start_time + 5.minutes
-			appointment.save
-			render status: 200, json: {
-				status: 200,
-				appointment: appointment
-			}.to_json
+		if Time.now < appointment.start_time
+			if appointment.update(appointment_params)
+				appointment.end_time = appointment.start_time + 5.minutes
+				appointment.save
+				render_json_success_with_appointment(appointment)
+			else
+				render_json_error(appointment.errors)
+			end
 		else
-			render status: 422, json: {
-				status: 422,
-				errors: appointment.errors
-			}.to_json
+			render_json_error("You can only make changes to future appointments")
 		end
 	end
 
@@ -38,29 +30,41 @@ class Api::AppointmentsController < ApplicationController
 		appointment = Appointment.new(appointment_params)
 		appointment.end_time = appointment.start_time + 5.minutes
 		if appointment.save
-			render status: 200, json: {
-				status: 200,
-				appointment: appointment
-			}.to_json
+			render_json_success_with_appointment(appointment)
 		else
-			render status: 422, json: {
-				status: 422,
-				errors: appointment.errors
-			}.to_json
+			render_json_error(appointment.errors)
 		end
 	end
 
 	def destroy
 		appointment = Appointment.find(params[:id])
 		appointment.destroy
-		render status: 200, json: {
-			status: 200,
-		}.to_json
+		render_json_success
 	end
 
 	private 
 
 	def appointment_params
 		params.require(:appointment).permit(:first_name, :last_name, :start_time, :end_time, :comments)
+	end
+
+	def render_json_error(error_msg)
+		render status: 422, json: {
+			status: 422,
+			errors: error_msg
+		}.to_json		
+	end
+
+	def render_json_success
+		render status: 200, json: {
+			status:200,
+		}.to_json
+	end
+
+	def render_json_success_with_appointment(appointment)
+		render status: 200, json: {
+			status: 200,
+			appointment: appointment
+		}.to_json
 	end
 end
