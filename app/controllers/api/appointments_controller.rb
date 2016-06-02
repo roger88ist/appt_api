a00c28274c009afefc67e13d2485e7aec94cdd3f
Rogerio Istiraneopulos <roger88ist@gmail.com>
2016-06-02 00:01:28 -0400

    added show method to api controller

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -3,4 +3,9 @@ class Api::AppointmentsController < ApplicationController
 		appointments = Appointment.all
 		render json: appointments
 	end
+
+	def show
+		appointment = Appointment.find(params[:id])
+		render json: appointment
+	end
 end
