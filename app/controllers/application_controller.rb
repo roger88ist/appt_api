8f101c6ee60d54d7294083dd18a8cb88ca991aec
Rogerio Istiraneopulos <roger88ist@gmail.com>
2016-06-05 20:33:57 -0400

    added rescue_from active record not being found in application_controller in case the client asks to search for an id that does not exit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+
+  def record_not_found
+  	render status: 422, json: {
+  		status: 422,
+  		errors: "Id provided does not match a record"
+  		}.to_json
+  end
+
 end
